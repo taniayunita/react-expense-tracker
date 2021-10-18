a9a06e57c152493172cae921101653f357ac0d19
taniayunita <taniayunita22@gmail.com>
2021-10-19 02:47:04 +0700

    add some components

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+//functional component with hooks
+
+import React from 'react'
+
+const Header = () => {
+    return (
+        <h2>
+            Expense Tracker
+        </h2>
+            
+    )
+}
+
+export default Header
